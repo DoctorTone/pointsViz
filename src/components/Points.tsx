621984fdc8db3fe6528c47bd9de0f1a8050b0368
DoctorTone <tony@drt-software.com>
2025-03-07 16:10:09 +0000

    Added new text formatting for axes

--- src/components/Points.tsx
+++ src/components/Points.tsx
@@ -1,14 +1,17 @@
-import { Html } from "@react-three/drei";
-import Typography from "@mui/material/Typography";
+import { Text } from "@react-three/drei";
 
 const Points = () => {
+  const pointsLine = [10, 20, 30, 40, 50, 60, 70];
   return (
     <>
-      <Html position={[0, 70, 0]}>
-        <Typography variant="h5" sx={{ color: "grey" }}>
-          Points
-        </Typography>
-      </Html>
+      <Text position={[-5, 80, 0]} fontSize={3} color="grey">
+        Points
+      </Text>
+      {pointsLine.map((total) => (
+        <Text position={[-5, total, 0]} fontSize={2} color="grey">
+          {total.toString()}
+        </Text>
+      ))}
     </>
   );
 };
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,29 +1,18 @@
-import { Html } from "@react-three/drei";
-import Typography from "@mui/material/Typography";
+import { Text } from "@react-three/drei";
 
 const Timeline = () => {
+  const matches = [5, 10, 15, 20, 25, 30, 35];
+
   return (
     <>
-      <Html position={[38 * 3, 0, 0]} transform={false} occlude={true}>
-        <Typography variant="h5" sx={{ color: "grey" }}>
-          Matches
-        </Typography>
-      </Html>
-      <Html position={[15, 0, 0]} transform={false} occlude={true}>
-        <Typography variant="h6" sx={{ color: "grey" }}>
-          5
-        </Typography>
-      </Html>
-      <Html position={[30, 0, 0]} transform={false} occlude={true}>
-        <Typography variant="h6" sx={{ color: "grey" }}>
-          10
-        </Typography>
-      </Html>
-      <Html position={[45, 0, 0]} transform={false} occlude={true}>
-        <Typography variant="h6" sx={{ color: "grey" }}>
-          15
-        </Typography>
-      </Html>
+      <Text color="grey" position={[40 * 3, 0, 0]} fontSize={3}>
+        Matches
+      </Text>
+      {matches.map((match) => (
+        <Text position={[match * 3, -1, 0]} fontSize={2} color="grey">
+          {match.toString()}
+        </Text>
+      ))}
     </>
   );
 };
