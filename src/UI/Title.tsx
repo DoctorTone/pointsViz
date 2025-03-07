91bf3882cedbecfad7c439049539790e71ec3fff
DoctorTone <tony@drt-software.com>
2025-03-07 19:30:23 +0000

    Formatted title and logo

--- src/UI/Title.tsx
+++ src/UI/Title.tsx
@@ -1,14 +1,22 @@
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid2";
 
 const Title = () => {
   return (
     <div id="title" className="panel">
-      <Typography variant="h4" sx={{ color: "white" }}>
-        Nottingham Forest
-      </Typography>
-      <Typography variant="h5" sx={{ color: "grey" }}>
-        Cumulative Points Total
-      </Typography>
+      <Grid container>
+        <Grid size={8} sx={{ mt: 1 }}>
+          <Typography variant="h4" sx={{ color: "white" }}>
+            Nottingham Forest
+          </Typography>
+          <Typography variant="h6" sx={{ color: "grey" }}>
+            Cumulative Points Total
+          </Typography>
+        </Grid>
+        <Grid size={2}>
+          <img className="w-50" src="./Images/forest.png" />
+        </Grid>
+      </Grid>
     </div>
   );
 };
