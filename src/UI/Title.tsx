b4ae21f8c4c7cd3c5d3bf5ff721098350b74b6d7
DoctorTone <tony@drt-software.com>
2025-03-10 23:34:08 +0000

    Moved title

--- src/UI/Title.tsx
+++ src/UI/Title.tsx
@@ -5,12 +5,9 @@ const Title = () => {
   return (
     <div id="title" className="panel">
       <Grid container>
-        <Grid size={8} sx={{ mt: 1, textAlign: "center" }}>
+        <Grid size={10} sx={{ mt: 2, textAlign: "center" }}>
           <Typography variant="h4" sx={{ color: "white" }}>
             Nottingham Forest
-          </Typography>
-          <Typography variant="h6" sx={{ color: "grey" }}>
-            Cumulative Points Total
           </Typography>
         </Grid>
         <Grid size={2}>
