e6f48b19e0c666678159700cd728a052f0bd34eb
DoctorTone <tony@drt-software.com>
2025-03-11 01:20:27 +0000

    Added data in json format

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -2,20 +2,15 @@ import { useMemo } from "react";
 import Segment from "./Segment";
 import { SegInfo } from "../Utils/SegmentInfo";
 import { SEGMENTS } from "../Utils/utils";
-
-const points_2324 = [0, 3, 3, 6, 7, 7, 8, 9, 10, 10];
-
-const points_2425 = [
-  1, 4, 5, 8, 9, 9, 10, 13, 16, 19, 19, 19, 22, 22, 25, 28, 31, 34, 37, 40, 41,
-  44, 44, 47, 47, 47, 48,
-];
+import points2425 from "../Data/forest_24_25.json";
+import points2324 from "../Data/forest_23_24.json";
 
 const Graph = () => {
   const segDetails: SegInfo[] = useMemo(() => {
     const details: SegInfo[] = [];
-    points_2425.map((_, index) => {
-      const nextPoints = points_2425[index];
-      const prevPoints = index === 0 ? 0 : points_2425[index - 1];
+    points2425.map((current, index) => {
+      const nextPoints = current.points;
+      const prevPoints = index === 0 ? 0 : points2425[index - 1].points;
       const deltaPoints = nextPoints - prevPoints;
       const segLength = Math.sqrt(9 + deltaPoints * deltaPoints);
       const rot = Math.asin(deltaPoints / segLength);
@@ -28,16 +23,14 @@ const Graph = () => {
       });
     });
 
-    // DEBUG
-    // console.log("Details = ", details);
     return details;
   }, []);
 
   const segDetails2: SegInfo[] = useMemo(() => {
     const details: SegInfo[] = [];
-    points_2324.map((_, index) => {
-      const nextPoints = points_2324[index];
-      const prevPoints = index === 0 ? 0 : points_2324[index - 1];
+    points2324.map((current, index) => {
+      const nextPoints = current.points;
+      const prevPoints = index === 0 ? 0 : points2324[index - 1].points;
       const deltaPoints = nextPoints - prevPoints;
       const segLength = Math.sqrt(9 + deltaPoints * deltaPoints);
       const rot = Math.asin(deltaPoints / segLength);
@@ -50,8 +43,6 @@ const Graph = () => {
       });
     });
 
-    // DEBUG
-    // console.log("Details = ", details);
     return details;
   }, []);
 
